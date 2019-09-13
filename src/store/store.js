be41b18e93b8038709a9176eeb975288e529d1f5
hcruz@solserin.com <hcruz@solserin.com>
2019-09-13 11:27:37 -0600

    vuex-persistedstate instalado

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import createPersistedState from 'vuex-persistedstate'
+
 import tokenManagement from './modules/token'
 import Perfil from './modules/perfil'
 import Status from './modules/status'
@@ -13,5 +15,6 @@ export const store=new Vuex.Store({
       tokenManagement,
       Perfil,
       Status
-    }
+    },
+    plugins: [createPersistedState()],
 })

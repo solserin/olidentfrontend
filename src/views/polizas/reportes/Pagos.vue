ac933cd91a04d15862b80bfbeed06901e7b335a9
Hector Cruz <hectorcrzprz@gmail.com>
2019-12-01 01:15:16 -0700

    terminado el reporte de cobranza desde el frontend

--- src/views/polizas/reportes/Pagos.vue
+++ src/views/polizas/reportes/Pagos.vue
@@ -108,11 +108,124 @@
                     <div>
                         <b-tabs content-class="mt-3" justified>
                             <b-tab title="Información Específica" active>
-                                <b-embed type="iframe" aspect="16by9" :src="this.pdf_url_especifico" allowfullscreen></b-embed>
+                                <!--<b-embed type="iframe" aspect="16by9" :src="this.pdf_url_especifico" allowfullscreen></b-embed>-->
+                                <div class="table-responsive-md">
+                                    <table class="table" v-for="(cobrador,index) in cobradores_id" v-bind:key="cobrador.id" v-bind:index="index">
+                                        <thead>
+                                            <tr style="background-color:#17a2b8; color:#fff;">
+                                                <th scope="col">COBRADOR: {{cobrador.nombre}}</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <th scope="row">
+                                                    <table class="table table-striped" style="font-weight:normal;">
+                                                        <thead>
+                                                            <tr>
+                                                                <th scope="col">#</th>
+                                                                <th scope="col">Fecha Abono</th>
+                                                                <th scope="col">Titular</th>
+                                                                <th scope="col">Capturista</th>
+                                                                <th scope="col">Ruta</th>
+                                                                <th scope="col"># Venta</th>
+                                                                <th scope="col">T. Venta</th>
+                                                                <th scope="col">T. Póliza</th>
+                                                                <th scope="col">Cantidad</th>
+                                                            </tr>
+                                                        </thead>
+                                                        <tbody>
+                                                            <tr :class="[abono.status?'':'eliminado']" v-for="(abono) in datos_result" v-bind:key="abono.id">
+                                                                <th v-if="cobrador.id==abono.id_cobrador" scope="row"><span>{{abono.id}}</span></th>
+                                                                <td v-if="cobrador.id==abono.id_cobrador"><span>{{abono.fecha_abono}}</span></td>
+                                                                <td v-if="cobrador.id==abono.id_cobrador"><span>{{abono.nombre}}</span></td>
+                                                                <td v-if="cobrador.id==abono.id_cobrador"><span>{{abono.capturista}}</span></td>
+                                                                <td v-if="cobrador.id==abono.id_cobrador"><span>{{abono.ruta}}</span></td>
+                                                                <td v-if="cobrador.id==abono.id_cobrador"><span>{{abono.ventaId}}</span></td>
+                                                                <td v-if="cobrador.id==abono.id_cobrador"><span>{{abono.tipoVenta}}</span></td>
+                                                                <td v-if="cobrador.id==abono.id_cobrador"><span>{{abono.tipoPoliza}}</span></td>
+                                                                <td v-if="cobrador.id==abono.id_cobrador"><span>$ {{ abono.cantidad | numFormat('0,000.00')}}</span></td>
+                                                            </tr>
+                                                            <tr style="border-bottom:3px solid #007bff; color:#007bff;">
+                                                                <td colspan="9" align="right"><strong>ABONOS COBRADOR POR: {{cobrador.nombre}}</strong></td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td colspan="9">
+                                                                    <table class="table">
+                                                                        <tbody>
+                                                                            <tr style="background-color:#fff; color:#000; font-weight:600;">
+                                                                                <td width="90%" align="right">
+                                                                                    Abonos Cobrados:
+                                                                                </td>
+                                                                                <td width="10%" align="right" style="background-color:#e9ecef; color:#007bff;">
+                                                                                    $ {{cobrador.cobrado | numFormat('0,000.00')}}
+                                                                                </td>
+                                                                            </tr>
+                                                                            <tr style="background-color:#fff; color:#000; font-weight:600;">
+                                                                                <td width="90%" align="right">
+                                                                                    Abonos Cancelados:
+                                                                                </td>
+                                                                                <td width="10%" align="right" style="background-color:#e9ecef; color:#dc3545;">
+                                                                                    $ {{cobrador.cancelado | numFormat('0,000.00')}}
+                                                                                </td>
+                                                                            </tr>
+                                                                            <tr style="background-color:#fff; color:#000; font-weight:600;">
+                                                                                <td width="90%" align="right">
+                                                                                    Total Cobrado:
+                                                                                </td>
+                                                                                <td width="10%" align="right" style="background-color:#e9ecef; color:#28a745;">
+                                                                                    $ {{cobrador.total | numFormat('0,000.00')}}
+                                                                                </td>
+                                                                            </tr>
+                                                                        </tbody>
+                                                                    </table>
+                                                                </td>
+                                                            </tr>
+
+                                                        </tbody>
+                                                    </table>
+                                                </th>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                    <div class="table-responsive-md">
+                                        <table class="table">
+                                            <tbody>
+                                                <tr style="background-color:#007bff; border-bottom:3px solid #007bff; color:#fff;">
+                                                    <td colspan="2" align="right"><strong>TOTAL DE INGRESOS POR COBRANZA:</strong></td>
+                                                </tr>
+                                                <tr style="background-color:#fff; color:#000; font-weight:600; font-size:16px;">
+                                                    <td width="90%" align="right">
+                                                        Abonos Cobrados:
+                                                    </td>
+                                                    <td width="10%" align="right" style="background-color:#e9ecef; color:#007bff;">
+                                                        $ {{this.cobrado | numFormat('0,000.00')}}
+                                                    </td>
+                                                </tr>
+                                                <tr style="background-color:#fff; color:#000; font-weight:600; font-size:16px;">
+                                                    <td width="90%" align="right">
+                                                        Abonos Cancelados:
+                                                    </td>
+                                                    <td width="10%" align="right" style="background-color:#e9ecef; color:#dc3545;">
+                                                        $ {{this.cancelado | numFormat('0,000.00')}}
+                                                    </td>
+                                                </tr>
+                                                <tr style="background-color:#fff; color:#000; font-weight:600; font-size:16px;">
+                                                    <td width="90%" align="right">
+                                                        Total Cobrado:
+                                                    </td>
+                                                    <td width="10%" align="right" style="background-color:#e9ecef; color:#28a745;">
+                                                        $ {{this.total | numFormat('0,000.00')}}
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+
+                                </div>
                             </b-tab>
-                            <b-tab title="Gráficas">
+                            <!--<b-tab title="Gráficas">
                                 <b-embed type="iframe" aspect="16by9" :src="this.pdf_url_especifico" allowfullscreen></b-embed>
-                            </b-tab>
+                            </b-tab>-->
 
                         </b-tabs>
                     </div>
@@ -136,6 +249,13 @@ export default {
                 showClose: true,
                 ignoreReadonly: true
             },
+            //con estos datos ordeno el resultado por cobrador
+            cobradores_id: [],
+            datos_result: [],
+            cobrado:0,
+            cancelado:0,
+            total:0,
+            //fin de ordenar datos
             pdf_url_especifico: '',
             polizas: [],
             pagos: [],
@@ -193,12 +313,75 @@ export default {
             try {
                 if (this.form.fecha_inicio && this.form.fecha_fin) {
                     this.$store.dispatch('loading');
-                    axios({
-                        url: this.$hostname + 'ventas/reporte_especifico_pagos?fecha_inicio=' + this.form.fecha_inicio + '&fecha_fin=' + this.form.fecha_fin + '&tipo_polizas_id=' + this.form.tipo_polizas_id + '&pagos_estado=' + this.form.pagos_estado + '&rutas_id=' + this.form.rutas_id + '&cobrador_id=' + this.form.cobrador_id + '&tipo_ventas_id=' + this.form.tipo_ventas_id+ '&capturo_id=' + this.form.capturo_id+ '&fecha_captura=' + this.form.fecha_captura,
-                        method: 'GET',
-                        responseType: 'blob',
-                    }).then((response) => {
-                        var fileURL = window.URL.createObjectURL(new Blob([response.data], {
+                    let url_query = "";
+                    if (this.form.fecha_captura) {
+                        url_query = this.$hostname + 'ventas/reporte_especifico_pagos?fecha_inicio=' + this.form.fecha_inicio + '&fecha_fin=' + this.form.fecha_fin + '&tipo_polizas_id=' + this.form.tipo_polizas_id + '&pagos_estado=' + this.form.pagos_estado + '&rutas_id=' + this.form.rutas_id + '&cobrador_id=' + this.form.cobrador_id + '&tipo_ventas_id=' + this.form.tipo_ventas_id + '&capturo_id=' + this.form.capturo_id + '&fecha_captura=' + this.form.fecha_captura
+                    } else {
+                        url_query = this.$hostname + 'ventas/reporte_especifico_pagos?fecha_inicio=' + this.form.fecha_inicio + '&fecha_fin=' + this.form.fecha_fin + '&tipo_polizas_id=' + this.form.tipo_polizas_id + '&pagos_estado=' + this.form.pagos_estado + '&rutas_id=' + this.form.rutas_id + '&cobrador_id=' + this.form.cobrador_id + '&tipo_ventas_id=' + this.form.tipo_ventas_id + '&capturo_id=' + this.form.capturo_id
+                    }
+                    axios.get(url_query).
+                    then((response) => {
+                        //reseteo los totales
+                        this.cobrado=0
+                        this.cancelado=0
+                        this.total=0
+                        //fin de reseteo
+                        this.cobradores_id = []
+                        this.$store.dispatch('success');
+                        this.datos_result = response.data
+
+                        let index_cobrador = 0;
+                        if (response.data.length) {
+                            this.cobradores_id.push({
+                                nombre: response.data[0].name,
+                                id: response.data[0].id_cobrador,
+                                cobrado: 0,
+                                cancelado: 0,
+                                total: 0
+                            });
+                        }
+                        for (let index = 0; index < response.data.length; index++) {
+                            if (index == 0) {
+                                //si solo es un row dejo el total al primer arreglo
+                                this.cobradores_id[index_cobrador].cobrado = response.data[index].cantidad
+                                if (response.data[index].status == 0) {
+                                    this.cobradores_id[index_cobrador].cancelado = response.data[index].cantidad
+                                }
+                                this.cobradores_id[index_cobrador].total = this.cobradores_id[index_cobrador].cobrado - this.cobradores_id[index_cobrador].cancelado
+                            } else {
+                                if (this.cobradores_id[index_cobrador].id == response.data[index].id_cobrador) {
+                                    this.cobradores_id[index_cobrador].cobrado += response.data[index].cantidad
+                                    if (response.data[index].status == 0) {
+                                        this.cobradores_id[index_cobrador].cancelado += response.data[index].cantidad
+                                    }
+                                    this.cobradores_id[index_cobrador].total = this.cobradores_id[index_cobrador].cobrado - this.cobradores_id[index_cobrador].cancelado
+                                }
+                                //sino es solo un row, se debe sumar todo row
+                                if (response.data[index + 1]) {
+                                    if (response.data[index + 1].id_cobrador != response.data[index].id_cobrador) {
+                                        index_cobrador++ //aumento el index del cobrador
+                                        this.cobradores_id.push({
+                                            nombre: response.data[index + 1].name,
+                                            id: response.data[index + 1].id_cobrador,
+                                            cobrado: 0,
+                                            cancelado: 0,
+                                            total: 0
+                                        });
+                                    }
+                                }
+
+                            }
+
+                        }
+
+                        //haciendo la suma total
+                          for (let index = 0; index < this.cobradores_id.length; index++) {
+                              this.cobrado+=this.cobradores_id[index].cobrado
+                              this.cancelado+=this.cobradores_id[index].cancelado
+                              this.total+=this.cobradores_id[index].total
+                          }
+
+                        /*var fileURL = window.URL.createObjectURL(new Blob([response.data], {
                             type: 'application/pdf'
                         }));
                         var fileLink = document.createElement('a');
@@ -206,8 +389,8 @@ export default {
                         fileLink.href = fileURL;
                         fileLink.setAttribute('download', 'file.pdf');
                         document.body.appendChild(fileLink);
-                        this.$store.dispatch('success');
                         //fileLink.click();
+                        */
                     }).catch(error => {
                         this.$toasted.show("Ha ocurrido un error en el servidor por favor reintente", {
                             iconPack: 'fontawesome',
@@ -221,17 +404,17 @@ export default {
                         });
                         this.$store.dispatch('error');
                     })
-                }else{
+                } else {
                     this.$toasted.show("Seleccione una fecha Incio y una fecha Fin", {
-                            iconPack: 'fontawesome',
-                            type: 'error',
-                            theme: 'toasted-primary',
-                            icon: 'close',
-                            duration: 4000,
-                            position: 'top-center',
-                            closeOnSwipe: true,
-                            keepOnHover: true
-                        });
+                        iconPack: 'fontawesome',
+                        type: 'error',
+                        theme: 'toasted-primary',
+                        icon: 'close',
+                        duration: 4000,
+                        position: 'top-center',
+                        closeOnSwipe: true,
+                        keepOnHover: true
+                    });
                 }
 
             } catch (error) {
@@ -244,7 +427,7 @@ export default {
         this.getVendedores();
         this.getRutasCobros()
         this.$store.dispatch('success')
-    },
+    }
 }
 </script>
 
